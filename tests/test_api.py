92827f40bc1afeb23c8b07f015d1db1aaaa93943
pyromakesmusic <pyromakesmusic@gmail.com>
2025-12-21 15:03:12 -0600

    fixed error with SQL deployment using static pool

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
+from sqlalchemy.pool import StaticPool
 from sqlalchemy.orm import sessionmaker
 
 from app import app, get_db
@@ -9,15 +10,18 @@ from models import Base
 SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    "sqlite://",
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
 )
+
 TestingSessionLocal = sessionmaker(
-    autocommit=False, autoflush=False, bind=engine
+    autocommit=False,
+    autoflush=False,
+    bind=engine
 )
 
 Base.metadata.create_all(bind=engine)
-
 
 def override_get_db():
     db = TestingSessionLocal()
