1bd07e30e57462bd4e6c8fb84993b21059b9aabc
pyromakesmusic <pyromakesmusic@gmail.com>
2026-03-22 02:51:33 -0500

    updated back to old db.py

--- repositories/db.py
+++ repositories/db.py
@@ -1,7 +1,11 @@
-from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-engine = create_engine("sqlite:///./test.db")
-SessionLocal = sessionmaker(bind=engine)
+DATABASE_URL = "sqlite:///./fitness.db"
+
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
