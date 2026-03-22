3c31375187895915ce779e6d02cf79381b99a4f6
pyromakesmusic <pyromakesmusic@gmail.com>
2026-03-22 02:46:23 -0500

    fixed db.py

--- repositories/db.py
+++ repositories/db.py
@@ -1,3 +1,7 @@
 from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+engine = create_engine("sqlite:///./test.db")
+SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
