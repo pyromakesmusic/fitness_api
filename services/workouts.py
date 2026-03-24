a1dddca6bde11c3e2e77d9c51eba16d9cdf1fdf9
pyromakesmusic <pyromakesmusic@gmail.com>
2026-03-24 16:29:22 -0500

    updating workouts.py to call all possible methods in both database backends

--- services/workouts.py
+++ services/workouts.py
@@ -1,15 +1,24 @@
 from repositories.base import workout_repo
 
-def create_workout(data):
-    return workout_repo.create_workout(data)
+def create_exercise(user_id, data):
+    return workout_repo.create_exercise(user_id, data)
+
+def get_exercise(user_id, exercise_id):
+    return workout_repo.get_exercise(user_id, exercise_id)
+
+def list_exercises(user_id):
+    return workout_repo.list_exercises(user_id)
+def create_workout(user_id, data):
+    return workout_repo.create_workout(user_id, data)
 
 def add_set(workout_id, data):
-    return workout_repo.add_exercise(workout_id, data)
+    return workout_repo.add_set(workout_id, data)
 
 def get_workout(workout_id):
     return workout_repo.get_workout(workout_id)
 
 def get_workouts_for_user(user_id):
     workouts = workout_repo.get_workouts_for_user(user_id)
-    # compute summaries here
+
+    # compute summaries here if needed
     return workouts
